6d970e26fc3ac79d35700a73546c09186d77fe67
jalzoren <flavierlaurence01@gmail.com>
2025-11-29 14:11:50 +0800

    aaaa

--- maicrafts/src/pages/About.jsx
+++ maicrafts/src/pages/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import "../css/About.css";
+
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      {/* HERO SECTION */}
+      <section className="hero-wrapper d-flex align-items-center justify-content-center">
+        <div className="hero-bg-circle"></div>
+        <div className="container hero-inner text-center">
+          <div className="row justify-content-end align-items-center">
+            <div className="col-md-6 hero-right-content text-md-end text-center">
+              <p className="hero-desc lead mb-4">
+                Order your Custom Gift Now!
+              </p>
+              <a href="/products" className="btn hero-btn-primary">
+                Customize!
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
 
-export default About
+    
+    </>
+  );
+};
+
+export default Products;
